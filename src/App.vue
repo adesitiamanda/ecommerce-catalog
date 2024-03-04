19e976dabbd8c7f5a4ab9a284c95886558bb1b09
Ade <140902040+adesitiamanda@users.noreply.github.com>
2024-03-04 09:10:33 +0700

    core initiative

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script>
+import ProductDisplay from './components/ProductDisplay.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ProductDisplay
+  }
+}
+</script>
+
+<template>
+  <ProductDisplay />
+</template>
+
+
